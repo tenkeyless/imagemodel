641cf4e01af7e316073006b5721ecea831669286
TenKeyLess <blivme84@naver.com>
2021-04-14 15:48:11 +0900

    + Add preprocessor

---
+++ imagemodel/binary_segmentations/datasets/bs_preprocessor.py
@@ -0,0 +1,69 @@
+from abc import ABCMeta
+from typing import Tuple
+
+from imagemodel.binary_segmentations.datasets.bs_preprocessor_helper import (
+    BaseBSPreprocessorInOutHelper,
+    BSPreprocessorInputHelper,
+    BSPreprocessorOutputHelper,
+)
+from imagemodel.common.datasets.manipulator.manipulator import SupervisedManipulator
+
+
+class BSPreprocessor(SupervisedManipulator, metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (
+            hasattr(subclass, "input_helper")
+            and callable(subclass.input_helper)
+            and hasattr(subclass, "output_helper")
+            and callable(subclass.output_helper)
+            or NotImplemented
+        )
+
+    @property
+    def input_helper(self) -> BSPreprocessorInputHelper:
+        pass
+
+    @property
+    def output_helper(self) -> BSPreprocessorOutputHelper:
+        pass
+
+
+class BaseBSPreprocessor(BSPreprocessor):
+    """
+    [summary]
+
+    Examples
+    --------
+    >>> from imagemodel.binary_segmentations.datasets.oxford_iiit_pet.feeder import BSOxfordIIITPetTrainingFeeder
+    >>> training_feeder = BSOxfordIIITPetTrainingFeeder()
+    >>> from imagemodel.binary_segmentations.datasets.bs_augmenter import BaseBSAugmenter
+    >>> augmenter = BaseBSAugmenter(manipulator=training_feeder)
+    >>> import cv2
+    >>> for index, inout in enumerate(augmenter.get_zipped_dataset().take(10)):
+    ...     input_dataset = inout[0]
+    ...     input_img = input_dataset[0]
+    ...     cv2.imwrite("augmented_img_{:02d}.png".format(index), input_img.numpy())
+    ...     output_dataset = inout[1]
+    ...     output_mask = output_dataset[0]
+    ...     cv2.imwrite("augmented_mask_{:02d}.png".format(index), output_mask.numpy()*255)
+    """
+
+    def __init__(
+        self,
+        manipulator: SupervisedManipulator,
+        height_width_tuple: Tuple[int, int] = (256, 256),
+    ):
+        self._manipulator: SupervisedManipulator = manipulator
+        self._inout_helper = BaseBSPreprocessorInOutHelper(
+            input_datasets=self._manipulator.get_input_dataset(),
+            output_datasets=self._manipulator.get_output_dataset(),
+        )
+
+    @property
+    def input_helper(self) -> BSPreprocessorInputHelper:
+        return self._inout_helper
+
+    @property
+    def output_helper(self) -> BSPreprocessorOutputHelper:
+        return self._inout_helper
---
+++ imagemodel/binary_segmentations/datasets/bs_preprocessor_helper.py
@@ -0,0 +1,106 @@
+from typing import Callable, List
+
+import tensorflow as tf
+from imagemodel.common.datasets.manipulator.helper import (
+    ManipulatorInputHelper,
+    ManipulatorOutputHelper,
+)
+
+
+class BSPreprocessorInputHelper(ManipulatorInputHelper):
+    """
+    <Interface>
+
+    Methods to get dataset for binary segmentation.
+    In this binary segmentation, we need raw image and corresponding mask.
+
+    Methods
+    -------
+    get_image() -> tf.data.Dataset
+        Image dataset. It returns `tf.data.Dataset`.
+        `height`, `width` and 0~255 range of tf.uint8.
+    """
+
+    def get_image_dataset(self) -> tf.data.Dataset:
+        """
+        Image dataset.
+        `height`, `width` and 0~255 range of tf.uint8.
+
+        Returns
+        -------
+        tf.data.Dataset
+            Dataset
+        """
+        pass
+
+    def image_preprocess_func(self) -> List[Callable[[tf.Tensor], tf.Tensor]]:
+        pass
+
+    def get_inputs(self) -> List[tf.data.Dataset]:
+        image_dataset = self.get_image_dataset()
+        for f in self.image_preprocess_func():
+            image_dataset = image_dataset.map(f)
+        return [image_dataset]
+
+
+class BSPreprocessorOutputHelper(ManipulatorOutputHelper):
+    """
+    <Interface>
+
+    Methods to get dataset for binary segmentation.
+    In this binary segmentation, we need raw image and corresponding mask.
+
+    Methods
+    -------
+    get_mask() -> tf.data.Dataset
+        Mask dataset. It returns `tf.data.Dataset`.
+        `height`, `width` and 0 or 255 value of tf.uint8.
+    """
+
+    def get_mask_dataset(self) -> tf.data.Dataset:
+        """
+        Mask dataset.
+        `height`, `width` and 0 or 255 value of tf.uint8.
+
+        Returns
+        -------
+        tf.data.Dataset
+            Dataset
+        """
+        pass
+
+    def mask_preprocess_func(self) -> List[Callable[[tf.Tensor], tf.Tensor]]:
+        pass
+
+    def get_outputs(self) -> List[tf.data.Dataset]:
+        mask_dataset = self.get_mask_dataset()
+        for f in self.mask_preprocess_func():
+            mask_dataset = mask_dataset.map(f)
+        return [mask_dataset]
+
+
+class BaseBSPreprocessorInOutHelper(
+    BSPreprocessorInputHelper, BSPreprocessorOutputHelper
+):
+    def __init__(
+        self,
+        input_datasets: List[tf.data.Dataset],
+        output_datasets: List[tf.data.Dataset],
+    ):
+        self._input_datasets: List[tf.data.Dataset] = input_datasets
+        self._output_datasets: List[tf.data.Dataset] = output_datasets
+
+    def get_image_dataset(self) -> tf.data.Dataset:
+        return self._input_datasets[0]
+
+    def image_preprocess_func(self) -> List[Callable[[tf.Tensor], tf.Tensor]]:
+        f1 = lambda img: tf.cast(img, tf.float32) / 255.0
+        return [f1]
+
+    def get_mask_dataset(self) -> tf.data.Dataset:
+        return self._output_datasets[0]
+
+    def mask_preprocess_func(self) -> List[Callable[[tf.Tensor], tf.Tensor]]:
+        f1 = lambda img: tf.cast(img, tf.float32)
+        f2 = lambda img: tf.cast(tf.greater(img, 0.5), tf.float32)
+        return [f1, f2]
