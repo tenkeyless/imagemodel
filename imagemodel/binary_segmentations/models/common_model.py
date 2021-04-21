009678ce209ffcad5d3dd22fadf5a7939fc7dc6b
TenKeyLess <blivme84@naver.com>
2021-04-21 16:02:25 +0900

    * Modify U-Net Level

---
+++ imagemodel/binary_segmentations/models/common_model.py
@@ -0,0 +1,26 @@
+import abc
+from abc import abstractmethod
+from typing import Generic, Dict, TypeVar
+
+from tensorflow.python.keras import Model
+from typing_extensions import TypedDict
+
+ST = TypeVar('ST', bound=TypedDict)
+
+
+class CommonModel(metaclass=abc.ABCMeta):
+    @abstractmethod
+    def setup_model(self) -> Model:
+        pass
+
+
+class CommonModelDictGeneratable(Generic[ST], metaclass=abc.ABCMeta):
+    @classmethod
+    @abstractmethod
+    def init_with_dict(cls, option_dict: ST):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def init_with_str_dict(cls, option_str_dict: Dict[str, str]):
+        pass
