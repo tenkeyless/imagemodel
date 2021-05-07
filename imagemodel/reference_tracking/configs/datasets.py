5b85cbf79e78876d4dcfcb60c5faee8f0b96f687
TenKeyLess <blivme84@naver.com>
2021-05-07 18:24:18 +0900

    + Add Tracking model, trainer, related layers

---
+++ imagemodel/reference_tracking/configs/datasets.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from typing import Callable, Optional, Tuple
+
+from imagemodel.common.datasets.pipeline import Pipeline
+from imagemodel.reference_tracking.datasets.cell_tracking.feeder import (
+    RTGSCellTrackingTrainingFeeder,
+    RTGSCellTrackingValidationFeeder
+)
+from imagemodel.reference_tracking.datasets.pipeline import RTPipeline
+from imagemodel.reference_tracking.datasets.rt_augmenter import RTAugmenter
+from imagemodel.reference_tracking.datasets.rt_regularizer import BaseRTRegularizer, RTRegularizer
+
+
+class Datasets(Enum):
+    rt_gs_cell_tracking_training_1 = "rt_gs_cell_tracking_training_1"
+    rt_gs_cell_tracking_validation_1 = "rt_gs_cell_tracking_validation_1"
+    none = "none"
+    
+    def get_pipeline(self, resize_to: Tuple[int, int]) -> Optional[Pipeline]:
+        regularizer_func: Callable[[RTAugmenter], RTRegularizer] = lambda el_bs_augmenter: BaseRTRegularizer(
+                el_bs_augmenter,
+                resize_to)
+        if self == Datasets.rt_gs_cell_tracking_training_1:
+            training_feeder = RTGSCellTrackingTrainingFeeder()
+            rt_training_pipeline = RTPipeline(training_feeder, regularizer_func=regularizer_func)
+            return rt_training_pipeline
+        elif self == Datasets.rt_gs_cell_tracking_validation_1:
+            validation_feeder = RTGSCellTrackingValidationFeeder()
+            rt_validation_pipeline = RTPipeline(validation_feeder, regularizer_func=regularizer_func)
+            return rt_validation_pipeline
+        else:
+            return None
+    
+    @staticmethod
+    def get_default() -> str:
+        return Datasets.none.value
---
+++ tests/test_reference_tracking/test_layers/test_extract_patch_layer.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+from imagemodel.reference_tracking.layers.extract_patch_layer import ExtractPatchLayer
+
+
+class TestExtractPatchLayer(tf.test.TestCase):
+    def test_extract_patch_layer_shapes(self):
+        batch_size = 2
+        hw_size = 2
+        channel_size = 3
+        k_size = 3
+        
+        input_feature = tf.ones([batch_size, hw_size, hw_size, channel_size])
+        output = ExtractPatchLayer(k_size=k_size)(input_feature)
+        
+        # assertion
+        tf.debugging.assert_shapes([(output, (batch_size, hw_size, hw_size, k_size * k_size * channel_size))])
+    
+    def test_sample(self):
+        c = tf.constant(np.arange(2 * 4 * 4 * 1).reshape((2, 4, 4, 1)), dtype=tf.float32)
+        epl = ExtractPatchLayer(k_size=5)(c)
+        print(epl)
