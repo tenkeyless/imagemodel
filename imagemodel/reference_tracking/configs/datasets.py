eed14bf14ff478a60757cc5cb8e3782f0d73f1ab
TenKeyLess <blivme84@naver.com>
2021-05-13 06:18:17 +0900

    - Remove single pipeline

--- imagemodel/reference_tracking/configs/datasets.py
+++ imagemodel/reference_tracking/configs/datasets.py
@@ -16,29 +16,13 @@ from imagemodel.reference_tracking.datasets.pipeline import RTPipeline
 from imagemodel.reference_tracking.datasets.rt_augmenter import RTAugmenter
 from imagemodel.reference_tracking.datasets.rt_preprocessor import RTPreprocessor
 from imagemodel.reference_tracking.datasets.rt_regularizer import BaseRTRegularizer, RTRegularizer
-from imagemodel.reference_tracking.datasets.single_pipeline.cell_tracking.feeder import (
-    RTCellTrackingTrainingFeeder as RTSingleCellTrackingTrainingFeeder,
-    RTCellTrackingValidationFeeder as RTSingleCellTrackingValidationFeeder,
-    RTGSCellTrackingTrainingFeeder as RTSingleGSCellTrackingTrainingFeeder,
-    RTGSCellTrackingValidationFeeder as RTSingleGSCellTrackingValidationFeeder
-)
-from imagemodel.reference_tracking.datasets.single_pipeline.pipeline import RTSinglePipeline
-from imagemodel.reference_tracking.datasets.single_pipeline.rt_augmenter import RTAugmenter as SingleRTAugmenter
-from imagemodel.reference_tracking.datasets.single_pipeline.rt_regularizer import (
-    BaseRTRegularizer as BaseSingleRTRegularizer,
-    RTRegularizer as SingleRTRegularizer
-)
 
 
 class Datasets(Enum):
     rt_cell_tracking_training_1 = "rt_cell_tracking_training_1"
     rt_cell_tracking_validation_1 = "rt_cell_tracking_validation_1"
-    rt_cell_tracking_training_2 = "rt_cell_tracking_training_2"
-    rt_cell_tracking_validation_2 = "rt_cell_tracking_validation_2"
     rt_gs_cell_tracking_training_1 = "rt_gs_cell_tracking_training_1"
     rt_gs_cell_tracking_validation_1 = "rt_gs_cell_tracking_validation_1"
-    rt_gs_cell_tracking_training_2 = "rt_gs_cell_tracking_training_2"
-    rt_gs_cell_tracking_validation_2 = "rt_gs_cell_tracking_validation_2"
     rt_cell_sample_test_1 = "rt_cell_sample_test_1"  # for Predict
     rt_cell_sample_2_test_1 = "rt_cell_sample_2_test_1"  # for Predict
     rt_gs_cell_sample_test_1 = "rt_gs_cell_sample_test_1"  # for Predict
@@ -57,18 +41,6 @@ class Datasets(Enum):
             validation_feeder = RTCellTrackingValidationFeeder()
             rt_validation_pipeline = RTPipeline(validation_feeder, regularizer_func=regularizer_func)
             return rt_validation_pipeline
-        elif self == Datasets.rt_cell_tracking_training_2:
-            regularizer_func: Callable[[SingleRTAugmenter], SingleRTRegularizer] = lambda \
-                    el_bs_augmenter: BaseSingleRTRegularizer(el_bs_augmenter, resize_to)
-            training_feeder = RTSingleCellTrackingTrainingFeeder()
-            rt_training_pipeline = RTSinglePipeline(training_feeder, regularizer_func=regularizer_func)
-            return rt_training_pipeline
-        elif self == Datasets.rt_cell_tracking_validation_2:
-            regularizer_func: Callable[[SingleRTAugmenter], SingleRTRegularizer] = lambda \
-                    el_bs_augmenter: BaseSingleRTRegularizer(el_bs_augmenter, resize_to)
-            validation_feeder = RTSingleCellTrackingValidationFeeder()
-            rt_validation_pipeline = RTSinglePipeline(validation_feeder, regularizer_func=regularizer_func)
-            return rt_validation_pipeline
         elif self == Datasets.rt_gs_cell_tracking_training_1:
             training_feeder = RTGSCellTrackingTrainingFeeder()
             rt_training_pipeline = RTPipeline(training_feeder, regularizer_func=regularizer_func)
@@ -76,18 +48,6 @@ class Datasets(Enum):
         elif self == Datasets.rt_gs_cell_tracking_validation_1:
             validation_feeder = RTGSCellTrackingValidationFeeder()
             rt_validation_pipeline = RTPipeline(validation_feeder, regularizer_func=regularizer_func)
-            return rt_validation_pipeline
-        elif self == Datasets.rt_gs_cell_tracking_training_2:
-            regularizer_func: Callable[[SingleRTAugmenter], SingleRTRegularizer] = lambda \
-                    el_bs_augmenter: BaseSingleRTRegularizer(el_bs_augmenter, resize_to)
-            training_feeder = RTSingleGSCellTrackingTrainingFeeder()
-            rt_training_pipeline = RTSinglePipeline(training_feeder, regularizer_func=regularizer_func)
-            return rt_training_pipeline
-        elif self == Datasets.rt_gs_cell_tracking_validation_2:
-            regularizer_func: Callable[[SingleRTAugmenter], SingleRTRegularizer] = lambda \
-                    el_bs_augmenter: BaseSingleRTRegularizer(el_bs_augmenter, resize_to)
-            validation_feeder = RTSingleGSCellTrackingValidationFeeder()
-            rt_validation_pipeline = RTSinglePipeline(validation_feeder, regularizer_func=regularizer_func)
             return rt_validation_pipeline
         elif self == Datasets.rt_cell_sample_test_1:
             preprocessor_func: Callable[[RTRegularizer], RTPreprocessor] = lambda \
